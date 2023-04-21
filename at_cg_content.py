4ac2a405cec192714c8271f3118b21ec9b5f2dd2
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 11:42:48 +0300

    Create at_cg_content.py

---
+++ at_cg_content.py
@@ -0,0 +1,5 @@
+# Extrancting the AT/CG content
+# Write a function that extracts the proportion (%) of a specific base in a DNA sequence.
+# The function must have two parameters:
+# 1. Sequence, i.e. the sequence to be analysed
+# 2. The base, i.e. A, T, C, G to be analysed
