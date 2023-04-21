67c44f5fc348f046fa8ba4ca7257a7a0a7d565a0
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 12:45:25 +0300

    Create 05_timing_algorithm.py

---
+++ 05_timing_algorithm.py
@@ -0,0 +1,4 @@
+# Timing algorithm execution
+# Use the previous random sequence generation function and the time module to
+# evaluate how many seconds the algorithm takes to generate a sequence with a thousand, ten thousand,
+# one hundred thousand and one million nucleotides (1,000, 10,000, 100,000, 1,000,000).
