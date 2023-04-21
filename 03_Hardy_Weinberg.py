96648902012e56d7130034ffea84f8a357b92a91
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 19:49:50 +0300

    Allele frequency and expected genotype

--- 03_Hardy_Weinberg.py
+++ 03_Hardy_Weinberg.py
@@ -23,16 +23,24 @@
 # Chi-square:
 #
 # x^2 = Σ[(Ο-Ε)^2/Ε]
-#Thus 6 parameters: PPobs, PQobs, QQobs, PPexp, PQexp, QQexp
+# Thus 6 parameters: PPobs, PQobs, QQobs, PPexp, PQexp, QQexp
 #
 # Explain the degrees of freedom.
 # Degrees of freedom: N. of genotypes - N. of alleles
 # Compare the measured chi-aquare with the table for the calculated degrees of freedom.
 
-def observed_allele_freq(pp_number, pq_number, qq_number):
-    p_freq = (2 * pp_number + pq_number) / (2 * (pp_number + pq_number + qq_number))
-    q_freq = (2 * qq_number + pq_number) / (2 * (pp_number + pq_number + qq_number))
-    return 'The observed allele P frequency is {:.5} and the allele Q frequency is {:.5}'.format(p_freq, q_freq)
+def allele_freq(pp_number, pq_number, qq_number):
+    population = pp_number + pq_number + qq_number
+    observed_p_freq = (2 * pp_number + pq_number) / (2 * population)
+    observed_q_freq = (2 * qq_number + pq_number) / (2 * population)
+    expected_pp_freq = pow(observed_p_freq, 2) * population
+    expected_pq_freq = 2 * population * observed_q_freq * observed_p_freq
+    expected_qq_freq = pow(observed_q_freq, 2) * population
 
-print(observed_allele_freq(1200,745,555))
+    # return observed_p_freq, observed_q_freq, expected_pq_freq, expected_pp_freq, expected_qq_freq
+    return 'The observed allele P frequency is {:.5} and the observed allele Q frequency is {:.5}.\nThe expected PP ' \
+           'genotype number is {:.5}, the expected PQ genotype number is {:.5} and the expected QQ genotype number is ' \
+           '{:.5}\n'.format(observed_q_freq, observed_q_freq, expected_pp_freq, expected_pq_freq, expected_qq_freq)
 
+
+print(allele_freq(1200, 745, 555))
