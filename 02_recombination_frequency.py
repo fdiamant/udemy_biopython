a22691fe645ff2c72ffb37c4147ee99e9c7140e3
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 18:16:10 +0300

    Solution

--- 02_recombination_frequency.py
+++ 02_recombination_frequency.py
@@ -1,3 +1,13 @@
 # The function of recombination frequency
-# Write a function that estimates the recombination frequency based on the number of recombinant and non-recombinant genotypes.
-# The function should take 4 parameters. Two for the recombinant and two for the non-recominant genotypes.
+# Write a function that estimates the recombination frequency based on the
+# number of recombinant and non-recombinant genotypes.
+# The function should take 4 parameters.
+# Two for the recombinant and two for the non-recombinant genotypes.
+
+def recombination_freq(rec1, rec2, nonrec1, nonrec2):
+    # freq = sum of recombinant / sum of all
+    percentage = (rec1 + rec2) / (rec1 + rec2 + nonrec1 + nonrec2)
+    return 'The recombination frequency is {:.2%}'.format(percentage)
+
+
+print(recombination_freq(4, 8, 54, 67))
