c56200dc276101f66d2dc1652aa683ed13daae45
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 11:54:34 +0300

    Create 03_Hardy_Weinberg.py

---
+++ 03_Hardy_Weinberg.py
@@ -0,0 +1,12 @@
+# A hypothetical population has 2500 individuals.
+# Scientists are investigating two alleles, P and Q.
+# The number of individuals with each genotype is as follows:
+#   PP: 1200
+#   PQ: 745
+#   QQ: 555
+# With the numbers above, calculate the frequency of each allele (0 to 1) and the chi-square for this population.
+#
+# Steps:
+# Calculate the frequency of each allele.
+# Calculate the expected number for each genotype.
+# Calculate the chi-square based on the number of observed and expected individuals
