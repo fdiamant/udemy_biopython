cd53b1a63b0a9c7e205f3cd8b0f287bcd202cefd
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 12:54:01 +0300

    Create allele_inheritance.py

---
+++ allele_inheritance.py
@@ -0,0 +1,15 @@
+# Allele inheritance
+#
+# Write code that simulates the reproduction between two parents (AA, Aa, aa) 
+# and show their offspring based on the probability of inheritance of each allele.
+# The program must also show the percentage of each genotype in the offspring.
+# 
+# Example:
+# Parent1: AA
+# Parent2: Aa
+# N. of offspring: 5
+#
+# AA  AA  AA  AA  AA
+# Aa  Aa  Aa  Aa  Aa
+# --  --  --  --  --
+# AA  Aa  AA  Aa  AA
