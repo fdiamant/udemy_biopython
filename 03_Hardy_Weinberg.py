8ba206148a11b468d79e36e1430f00f7c946a1be
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 19:20:56 +0300

    Allele frequency

--- 03_Hardy_Weinberg.py
+++ 03_Hardy_Weinberg.py
@@ -12,8 +12,8 @@
 # Calculate the chi-square based on the number of observed and expected individuals
 #
 # Allele frequency:
-# (2 x homozigotes x heterozigotes)/2 x N
-# The the function needs to take three parameters.
+# (2 x homozigotes + heterozigotes)/2 x N
+# The function needs to take three parameters.
 #
 # Expected number of each genotype:
 # ExpPP = freqP^2 x N
@@ -28,3 +28,11 @@
 # Explain the degrees of freedom.
 # Degrees of freedom: N. of genotypes - N. of alleles
 # Compare the measured chi-aquare with the table for the calculated degrees of freedom.
+
+def observed_allele_freq(pp_number, pq_number, qq_number):
+    p_freq = (2 * pp_number + pq_number) / (2 * (pp_number + pq_number + qq_number))
+    q_freq = (2 * qq_number + pq_number) / (2 * (pp_number + pq_number + qq_number))
+    return 'The observed allele P frequency is {:.5} and the allele Q frequency is {:.5}'.format(p_freq, q_freq)
+
+print(observed_allele_freq(1200,745,555))
+
