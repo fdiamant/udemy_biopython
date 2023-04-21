af410d492d9ca255818d20cb96f69834db8b70ad
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 17:54:37 +0300

    Solution

--- 01_at_cg_content.py
+++ 01_at_cg_content.py
@@ -6,14 +6,23 @@
 
 def base_proportion(seq, base):
     # Calculate the proportion
-    # Count the occurences of the specified base and divide them by the string (sequence) length
-    proportion = seq.count(base) / len(seq)
+    # Count the occurences of the specified base
+    first_base_count = seq.count(base)
+    # Find the second base of the AT/CG pair
+    pairing = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
+    second_base = pairing[base]
+    # Count the occurences of the second base
+    second_base_count = seq.count(second_base)
+    # Calculate the total count
+    total_count = first_base_count + second_base_count
+    # Calculate the proportion
+    proportion = total_count / len(seq)
     # Format the output.
     # The calculated proportion is turned into percentage format with two decimals
     return'The proportion of the base {} in the given sequence is {:.2%}'.format(base, proportion)
 
 
-test_seq = 'ATCGTC'
-test_base = 'A'
+test_seq = 'ATCCAGCCTGAGAGATGGGAGAATTGATACGGGACTCTACGTACGTAACTCTCGGTTCGTAGACACCCGACAGCCCGTCTCGCACAGGAGCTATTTATGG'
+test_base = 'G'
 
 print(base_proportion(test_seq, test_base))
