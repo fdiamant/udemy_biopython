0b41d4d52b3a6e09af8d7ea7bc4de8a71c8a7ecb
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 11:48:16 +0300

    Create 02_recombination_frequency.py

---
+++ 02_recombination_frequency.py
@@ -0,0 +1,3 @@
+# The function of recombination frequency
+# Write a function that estimates the recombination frequency based on the number of recombinant and non-recombinant genotypes.
+# The function should take 4 parameters. Two for the recombinant and two for the non-recominant genotypes.
