38f24137cf64cdc59c1f24416995bc2fa620ca6f
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-21 12:13:39 +0300

    Update 03_Hardy_Weinberg.py

--- 03_Hardy_Weinberg.py
+++ 03_Hardy_Weinberg.py
@@ -10,3 +10,17 @@
 # Calculate the frequency of each allele.
 # Calculate the expected number for each genotype.
 # Calculate the chi-square based on the number of observed and expected individuals
+#
+# Allele frequency:
+# (2 x homozigotes x heterozigotes)/2 x N
+# The the function needs to take three parameters.
+#
+# Expected number of each genotype:
+# ExpPP = freqP^2 x N
+# ExpPQ = freqP x freqQ x N x 2
+# ExpQQ = freqQ^2 x N
+#
+# Chi-square:
+#
+# x^2 = Σ[(Ο-Ε)^2/Ε]
+#Thus 6 parameters: PPobs, PQobs, QQobs, PPexp, PQexp, QQexp
