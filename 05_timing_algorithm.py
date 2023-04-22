97e50bc09a86009e87836a945af0620668c669ca
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-22 16:27:32 +0300

    Solution

--- 05_timing_algorithm.py
+++ 05_timing_algorithm.py
@@ -5,7 +5,6 @@
 
 import time
 import random
-from colorama import init, Fore, Style
 from tabulate import tabulate
 
 
@@ -33,17 +32,7 @@ def random_dna_seq(length):
         # Choose a random base from the base list
         dna_seq += random.choice(base_list)
         i += 1
-    # Initialize colorama
-    init()
-    # Set the colours for each base
-    base_colors = {'A': Fore.RED, 'T': Fore.GREEN, 'C': Fore.YELLOW, 'G': Fore.BLUE}
-    # Color each base and define the colorama style as bright
-    # The end='' eliminates the space between the characters
-    for base in dna_seq:
-        color = base_colors.get(base, Fore.WHITE)
-        print(color + base + Style.BRIGHT, end='')
-    # Reset colorama styles
-    print(Style.RESET_ALL)
+    return dna_seq
 
 
 # Example
