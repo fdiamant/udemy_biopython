0ffb8bb7c827153b88e252e75a3b0f932ea4fe76
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-22 17:53:21 +0300

    solution

--- 06_allele_inheritance.py
+++ 06_allele_inheritance.py
@@ -16,14 +16,30 @@ def reproduction_sim(genotype1, genotype2, offspring_number):
         # Create an empty list to store the offsprings
         offsprings = []
         # Repeat for the wanted number of offsprings
-        for allele in range(offspring_number):
+        for _ in range(offspring_number):
             # Choose a random allele from the first parent and a random allele from the second parent
             # Join them to create the offspring and store the new offspring in the offsprings list
-            offspring = random.choice(genotype1) + random.choice(genotype2)
+            # aA and Aa are the same
+            allele1 = random.choice(genotype1)
+            allele2 = random.choice(genotype2)
+            if allele1 == 'a' and allele2 == 'A':
+                offspring = 'Aa'
+            elif allele1 == 'A' and allele2 == 'a':
+                offspring = 'Aa'
+            else:
+                offspring = allele1 + allele2
             offsprings.append(offspring)
-        return offsprings
 
-print(reproduction_sim('aa', 'aa', 10))
+    # Calculate percentages
+    percentages = {}
+    for offspring in offsprings:
+        percentage = offsprings.count(offspring) / len(offsprings)
+        percentages[offspring] = percentage
 
-# mylist = ["apple", "banana", "cherry"]
-# print(random.choices(mylist, weights = [10, 1, 1], k = 14))
+
+    return percentages
+
+
+
+print(reproduction_sim('Aa', 'Aa', 10))
+
