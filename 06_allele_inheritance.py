343c26395eb7516618f24fdefc24cae0a04c4a80
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-22 17:13:19 +0300

    Comments

--- 06_allele_inheritance.py
+++ 06_allele_inheritance.py
@@ -8,12 +8,17 @@
 import random
 
 def reproduction_sim(genotype1, genotype2, offspring_number):
+    # Check if the given genotypes are valid
     valid = {'AA', 'Aa', 'aa'}
     if genotype1 not in valid or genotype2 not in valid:
         raise ValueError("results: genotype must be one of %r." % valid)
     else:
+        # Create an empty list to store the offsprings
         offsprings = []
+        # Repeat for the wanted number of offsprings
         for allele in range(offspring_number):
+            # Choose a random allele from the first parent and a random allele from the second parent
+            # Join them to create the offspring and store the new offspring in the offsprings list
             offspring = random.choice(genotype1) + random.choice(genotype2)
             offsprings.append(offspring)
         return offsprings
