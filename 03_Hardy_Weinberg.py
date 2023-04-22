1f0faf0512f1d254c7f12e2e2a4955a81b94496f
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-22 09:23:02 +0300

    Second iteration

--- 03_Hardy_Weinberg.py
+++ 03_Hardy_Weinberg.py
@@ -11,10 +11,6 @@
 # Calculate the expected number for each genotype.
 # Calculate the chi-square based on the number of observed and expected individuals
 #
-# Allele frequency:
-# (2 x homozigotes + heterozigotes)/2 x N
-# The function needs to take three parameters.
-#
 # Expected number of each genotype:
 # ExpPP = freqP^2 x N
 # ExpPQ = freqP x freqQ x N x 2
@@ -29,29 +25,66 @@
 # Degrees of freedom: N. of genotypes - N. of alleles
 # Compare the measured chi-aquare with the table for the calculated degrees of freedom.
 
-import scipy.stats
+
+# Retrieve the observed genotype numbers of the population
+pp_obs = int(input('Please input the count of entities possessing the PP genotype: '))
+pq_obs = int(input('Please input the count of entities possessing the PQ genotype: '))
+qq_obs = int(input('Please input the count of entities possessing the QQ genotype: '))
+
+# Calculate the population number
+n = pp_obs + pq_obs + qq_obs
+obs = [pp_obs, pq_obs, qq_obs]
+
+print(
+    '\nThe population is {0}.\nThe number of individuals with PP genotype are {1}.\nThe number of individuals with PQ '
+    'genotype are {2}.\nThe number of individuals with QQ genotype are {3}.\n'.format(
+        n, pp_obs, pq_obs, qq_obs))
 
 
-def allele_freq(pp_number, pq_number, qq_number):
-    population = pp_number + pq_number + qq_number
-    observed_p_freq = (2 * pp_number + pq_number) / (2 * population)
-    observed_q_freq = (2 * qq_number + pq_number) / (2 * population)
-    expected_pp_number = pow(observed_p_freq, 2) * population
-    expected_pq_number = 2 * population * observed_q_freq * observed_p_freq
-    expected_qq_number = pow(observed_q_freq, 2) * population
-
-    chi_square = (pow(pp_number - expected_pp_number, 2) / expected_pp_number) + (
-            pow(pq_number - expected_pq_number, 2) / expected_pq_number) + (
-                         pow(qq_number - expected_qq_number, 2) / expected_qq_number)
-
-    table_chi = scipy.stats.chi2.ppf(1 - .001, df=1)
-
-    return 'The observed allele P frequency is {:.5} and the observed allele Q frequency is {:.5}.\nThe expected PP ' \
-           'genotype number is {:.5}, the expected PQ genotype number is {:.5} and the expected QQ genotype number is ' \
-           '{:.5}\nThe chi_square is {:.5} which is larger than {:.5} for significance level 0.001.\nThus the ' \
-           'hypothesis stands with 99.9% certainty.'.format(
-        observed_q_freq, observed_q_freq, expected_pp_number, expected_pq_number, expected_qq_number, chi_square,
-        table_chi)
+# Calculate the frequencies of P and Q in the population
+# Allele frequency = (2 x homozigotes + heterozigotes)/2 x population
+def allele_freq(homozigotes, heterozigotes, population):
+    return (2 * homozigotes + heterozigotes) / (2 * population)
 
 
-print(allele_freq(1200, 745, 555))
+p_freq = allele_freq(pp_obs, pq_obs, n)
+q_freq = allele_freq(qq_obs, pq_obs, n)
+
+print('The allele P frequency is {}.\nThe allele Q frequency is {}'.format(p_freq, q_freq))
+
+# def hypothesis_testing():
+#
+#
+#     p_freq = (2 * pp_obs + pq_obs) / (2 * n)
+#     q_freq = (2 * qq_obs + pq_obs) / (2 * n)
+#
+#     # Calculate the expected number of individuals for each genotype
+#     pp_exp = pow(p_freq, 2) * n
+#     pq_exp = 2 * n * q_freq * p_freq
+#     qq_exp = pow(q_freq, 2) * n
+#
+#     # Assign the observed and expected results in two lists
+#     gen_obs = [pp_obs, pq_obs, qq_obs]
+#     gen_exp = [pp_exp, pq_exp, qq_exp]
+#
+#     # Calculate the chisquare
+#     chi_sq = 0
+#     i=0
+#     while i <= len(gen_obs):
+#         chi_sq += (pow(gen_obs - gen_exp), 2) / gen_exp
+#         i+=1
+#
+#     chi_square = (pow(pp_obs - pp_exp, 2) / pp_exp) + (
+#             pow(pq_obs - pq_exp, 2) / pq_exp) + (
+#                          pow(qq_obs - qq_exp, 2) / qq_exp)
+#     return chi_square, chi_sq
+#
+#
+# # return 'The observed allele P frequency is {:.5} and the observed allele Q frequency is {:.5}.\nThe expected PP '
+# \ #        'genotype number is {:.5}, the expected PQ genotype number is {:.5} and the expected QQ genotype number
+# is ' \ #        '{:.5}\nThe chi_square is {:.5} which is larger than {:.5} for significance level 0.001.\nThus the
+# ' \ #        'hypothesis stands with 99.9% certainty.'.format( #     p_freq, q_freq, pp_exp, pq_exp, qq_exp,
+# chi_square, #     table_chi)
+#
+#
+# print(hypothesis_testing())
