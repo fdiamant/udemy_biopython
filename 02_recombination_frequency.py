f983b233e5f47a18f6ab11fc48543d812efa5fed
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-23 09:23:20 +0300

    Try except

--- 02_recombination_frequency.py
+++ 02_recombination_frequency.py
@@ -4,10 +4,30 @@
 # The function should take 4 parameters.
 # Two for the recombinant and two for the non-recombinant genotypes.
 
-def recombination_freq(rec1, rec2, nonrec1, nonrec2):
+
+def get_genotype_num(prompt="Enter number: "):
+
+    # (str) -> num
+    # Outputs a prompt and loops until a valid number is
+    # entered by the user, at which point that value is
+    # returned and the function terminates
+
+    while True:
+        num = input(prompt)
+        try:
+            num = int(num)
+            return num
+        except:
+            print("Please enter a valid number (e.g., 1, 134, 56, 79).")
+
+def recombination_freq():
+    rec1 = get_genotype_num('Number of recombinant genotype 1:')
+    rec2 = get_genotype_num('Number of recombinant genotype 2:')
+    nonrec1 = get_genotype_num('Number of non-recombinant genotype 1:')
+    nonrec2 = get_genotype_num('Number of non-recombinant genotype 2:')
     # freq = sum of recombinant / sum of all
     percentage = (rec1 + rec2) / (rec1 + rec2 + nonrec1 + nonrec2)
     return 'The recombination frequency is {:.5%}'.format(percentage)
 
 
-print(recombination_freq(4, 8, 54, 67))
+print(recombination_freq())
