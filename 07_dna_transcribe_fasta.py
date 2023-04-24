f174ba1dbcdbbb5488093d250e33c56405094d1f
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-24 11:46:41 +0300

    Transcribe FASTA to txt solution

--- 07_dna_transcribe_fasta.py
+++ 07_dna_transcribe_fasta.py
@@ -2,3 +2,28 @@
 1. Go to some gene/genome repository and download some FASTA file of a DNA sequence.
 2. Import the file.
 3. Generate a .txt file containing the transcribed sequence."""
+
+
+# Download Staphylococcus aureus subsp. aureus NCTC 8325 chromosome, complete genome
+# NCBI Reference Sequence: NC_007795.1
+# from NCBI db
+
+def open_seq():
+    with open('sequence.fasta', 'r') as gen_seq:
+        # Skip the first line
+        gen_seq.readline()
+        # Create a list with the rest of the lines
+        seq_list = gen_seq.readlines()
+        # Return a string from the created list
+        return ''.join(seq_list)
+
+
+def write_file():
+    # Create a new file with write permission
+    with open('transcribed_seq.txt', 'w') as transcribed_seq:
+        # Write the output of the open_seq function into the file
+        transcribed_seq.write(open_seq())
+
+# Example
+write_file()
+
