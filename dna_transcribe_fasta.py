34dda6f9af36a88839ba1dff0d34e522050421e7
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-24 10:00:43 +0300

    Update and rename dna_transcribe_fasta.py to 07_dna_transcribe_fasta.py

--- dna_transcribe_fasta.py
+++
@@ -1,4 +0,0 @@
-### Transcribing a DNA sequence from a FASTA file
-1. Go to some gene/genome repository and download some FASTA file of a DNA sequence.
-2. Import the file.
-3. Generate a .txt file containing the transcribed sequence. ###
