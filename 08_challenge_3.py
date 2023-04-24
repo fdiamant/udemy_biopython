8d7e8d04d8e620b7dab5669c5b76dbf825d8d9df
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-24 13:20:25 +0300

    Create 08_challenge_3.py

---
+++ 08_challenge_3.py
@@ -0,0 +1,13 @@
+"""
+Manipulating a genetic sequence from a database
+
+Write a python script that reads a genetic sequence from a database.
+The program must display a menu to the user, allowing her/him to choose from the folloing options:
+
+1. Return the RNA sequence and write it to a file.
+2. Return the complementary sequence and write it to a file.
+3. Show the A-T content.
+4. Show the C-G content.
+
+Make a error treatment code, so that the program warns the user about an invalid option.
+"""
